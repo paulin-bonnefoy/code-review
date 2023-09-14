9da30bc32f260e3bd118a42340a6445bf69a1d23
LucasBarreau <lbarreau@takima.fr>
2023-09-14 15:45:59 +0200

    home servlet

---
+++ src/main/java/com/example/codereview/HomeServlet.java
@@ -0,0 +1,28 @@
+package com.example.codereview;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello plp!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
