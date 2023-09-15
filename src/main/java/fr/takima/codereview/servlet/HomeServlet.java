7e2268c17e69fe443220005c8987d84b4002f873
Paulin <pbonnefoy@takima.fr>
2023-09-15 11:12:31 +0200

    Better architecture

--- src/main/java/fr/takima/codereview/servlet/HomeServlet.java
+++ src/main/java/fr/takima/codereview/servlet/HomeServlet.java
@@ -10,12 +10,8 @@ import jakarta.servlet.annotation.*;
 public class HomeServlet extends HttpServlet {
     private String message;
 
-    public void init() {
-        message = "Hello plp!";
-    }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        this.getServletContext().getRequestDispatcher("/resources/views/home.jsp").forward(request, response);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
     }
 
     public void destroy() {
